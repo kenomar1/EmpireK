3b909099d07bbdd490503221d08edcb0b030ea68
smokeparadise.us <smokeparadise.us@gmail.com>
2025-12-14 05:29:26 +0200

    new sender

--- api/send-email.js
+++ api/send-email.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
   try {
     await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
+      from: "Contact Form <primary1omar@gmail.com>",
       to: "dev.empirek@hotmail.com",
       reply_to: email,
       subject: `New message from ${name}`,
