5a174304e4a093b5063124afb52a5acd83f27d4e
Kendrick Omar <primary1omar@gmail.com>
2025-12-28 19:06:32 +0200

    still

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+    },
+  },
   build: {
     outDir: "dist",
     sourcemap: false,
